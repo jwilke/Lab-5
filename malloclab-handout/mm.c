9d2aa72c7d9f24a2f35a87f4ef7c430884acc583
Graham Benevelli <grahambenevelli@gmail.com>
2012-03-02 09:35:21 -0600

    Nodes Testing Done

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -86,7 +86,7 @@ team_t team = {
 #define GET_RIGHT(p)		GET(MC(p)-2*(WSIZE))
 #define GET_ALLOC_T(p)		(GET(MC(p)-1*(WSIZE)) & 0x1)
 #define GET_SIZE_T(p)		(GET(MC(p)-1*(WSIZE)) & ~0x3)
-#define GET_RB(p)		(GET(MC(p)-1*(WSIZE)) & 0x2)
+#define GET_RB(p)		((GET(MC(p)-1*(WSIZE)) & 0x2) >> 1)
 #define GET_PARENT(p)		GET(MC(p)+GET_SIZE_T(p))
 //#define GET_LAST(p)		((GET_LEFT( GET(MC(p)-4*WSIZE)) == p ) ? (GET_LEFT(GET(MC(p)-4*WSIZE))) : (GET_RIGHT(GET(MC(p)-4*WSIZE))))
 #define GET_LAST(p)		get_last(p)
